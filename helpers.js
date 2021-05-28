23735c5e728056aeacc9907f00a3bd7c32949b57
JettScythe <jettscythe@hotmail.com>
2021-05-28 20:12:13 -0300

    add helpers.js to auth users

---
+++ helpers.js
@@ -0,0 +1,42 @@
+const argon2 = require('argon2');
+const { Pool } = require('pg');
+const dbParams = require('./lib/db.js');
+const db = new Pool(dbParams);
+db.connect();
+
+
+const addNewUser = async function(user) {
+  const hash = await argon2.hash(user.user_pass);
+  try {
+    const newUser = await db.query(`INSERT INTO users(NAME, EMAIL, USER_PASS, ORGANIZATION_ID) 
+    VALUES ($1, $2, $3, $4) RETURNING *;`, [user.name, user.email, hash, user.organization_id])
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+exports.addNewUser = addNewUser;
+
+const getUserByEmail = async function(email) {
+  try {
+    const user = await db.query(`SELECT * FROM users WHERE email = $1`, [email])
+  } catch (err) {
+    console.log(err.message)
+  }
+};
+exports.getUserByEmail = getUserByEmail;
+
+const authenticateUser = async function(email, password) {
+  // retrieve the user with that email
+  try {
+    let user = await getUserByEmail(email);
+    if (await argon2.verify(user.user_pass, password)) {
+      return user
+    } else {
+      user = null;
+      return user;
+    }
+  } catch (err) {
+    console.log(err.message)
+  }
+};
+exports.authenticateUser = authenticateUser;
