dc71174fbd77457b3f18013eada274cb4775211f
Zain Dewsi <zain.dewsi@gmail.com>
2021-05-28 18:48:49 -0400

    create register route

--- server.js
+++ server.js
@@ -37,11 +37,13 @@ app.use(express.static("public"));
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
+const registerRoutes = require("./routes/register");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 // Note: mount other resources here, using the same pattern above
+app.use("/register", registerRoutes());
 
 // Home page
 // Warning: avoid creating more routes in this file!
