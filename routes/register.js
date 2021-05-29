2ac211f4be8824ba8355a10afef3bca8686b077a
JettScythe <jettscythe@hotmail.com>
2021-05-29 11:56:58 -0300

    temp remove ejs in headers partial

--- routes/register.js
+++ routes/register.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const addNewUser = require("../helpers")
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
     res.render("register");
   });
   router.post("/", (req, res) => {
-    console.log("new user");
+    addNewUser();
     res.redirect("/");
   });
   return router;
 };
+
+
