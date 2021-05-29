03cb3b31ea4716f41af847b5e4ed34d44c5f3f15
Zain Dewsi <zain.dewsi@gmail.com>
2021-05-29 10:39:48 -0400

    create post route for registration form

--- routes/register.js
+++ routes/register.js
@@ -5,5 +5,9 @@ module.exports = (db) => {
   router.get("/", (req, res) => {
     res.render("register");
   });
+  router.post("/", (req, res) => {
+    console.log("new user");
+    res.redirect("/");
+  });
   return router;
 };
