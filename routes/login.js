a6772cabd0a55f852f1d24bb6d2a977e47f26293
Zain Dewsi <zain.dewsi@gmail.com>
2021-05-30 10:02:04 -0400

    modify login route to redirect to passwords page

--- routes/login.js
+++ routes/login.js
@@ -1,24 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 module.exports = (obj) => {
+  router.post("/", (req, res) => {
+    const { email, password } = req.body;
+    obj
+      .authenticateUser(email, password)
+      .then((user) => {
+        console.log("user:", user);
+        if (!user) {
+          res.render("index.ejs", { error: true });
+          return;
+        }
+        req.session["user_id"] = user.id;
+        req.session["organization_id"] = user.organization_id;
 
-  router.post('/', (req, res) => {
-    const {email, password} = req.body;
-    obj.authenticateUser(email, password)
-    .then(user => {
-      console.log('user:', user);
-      if (!user) {
-        res.render('index.ejs', {error: true});
-        return;
-      }
-      req.session['user_id'] = user.id;
-      req.session['organization_id'] = user.organization_id;
-      res.send(user);
-      console.log('logged in successfully');
-      res.redirect('/passwords');
-    })
-    .catch(e => res.send(e));
-  })
+        res.redirect("/passwords");
+      })
+      .catch((e) => res.send(e));
+  });
   return router;
-}
+};
