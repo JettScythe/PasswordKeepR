0cbb0823cd0ffa4504dd6aadb73e58cfe4b933fa
Moses Kim <moseskim4740@gmail.com>
2021-05-31 21:53:06 +0000

    you can edit individual passwords now

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -9,11 +9,11 @@ const renderPasswords = () => {$.ajax({
             <p>username: ${password.website_username}</p>
             <p>password: ${password.website_password}</p>
             <button class="btn btn-primary toggle_edit" type="button">edit</button>
-            <form method="post" autocomplete="off" autofill="off" class='edit_info_${index}'>
+            <form method="POST" autocomplete="off" autofill="off" class='edit_info_${index}'>
               <div class="form-group">
                 <input
                   type="text"
-                  class="web_name"
+                  class="web_name_${index}"
                   name="website_name"
                   placeholder="Website Name"
                 />
@@ -21,7 +21,7 @@ const renderPasswords = () => {$.ajax({
               <div class="form-group">
                 <input
                   type="text"
-                  class="username"
+                  class="username_${index}"
                   name="website_username"
                   placeholder="username"
                 />
@@ -29,7 +29,7 @@ const renderPasswords = () => {$.ajax({
               <div class="form-group">
                 <input
                   type="password"
-                  class="password"
+                  class="password_${index}"
                   name="website_password"
                   placeholder="password"
                 />
@@ -50,9 +50,6 @@ $('.toggle_add_account').on('click', () => {
 });
 
 $('.add_new').submit(event => {
-  // alert('succes...');
-  // console.log('this works');
-  // const data = $(this).serialize();
   const name = $('#web_name').val();
   const username = $('#web_username').val();
   const password = $('#new_password').val();
@@ -66,24 +63,30 @@ $('.add_new').submit(event => {
   })
 })
 
-$('.passwords_container').on('submit', '.edit_info', (event) => {
-  event.preventDefault();
-  const web_name = $('.web_name').val();
-  const username = $('.username').val();
-  const password = $('.password').val();
-  console.log('submitted edit info form successfully');
-  console.log('from within ajax:', web_name, username, password);
-  $.post('/passwords/edit', { web_name, username, password })
-  .then((res) => {
-    $('.passwords_container').empty();
-    renderPasswords();
-  }).catch(err => {
-    console.log('error from edit password request:', err);
-  })
-});
 
 
 $('.passwords_container').on('click', '.toggle_edit', (event) => {
-  console.log(event.currentTarget.nextElementSibling);
-  return $(event.currentTarget.nextElementSibling).slideToggle();
+  $(event.currentTarget.nextElementSibling).slideToggle();
+
+  const form = $(event.currentTarget.nextElementSibling).attr('class');
+  const index = form.slice(-1);
+  console.log('index:', index);
+
+  $('.passwords_container').on('submit', `.edit_info_${index}`, (event) => {
+    event.preventDefault();
+    console.log(`this event is: ${event.currentTarget}`)
+    const web_name = $(`.web_name_${index}`).val();
+    const username = $(`.username_${index}`).val();
+    const password = $(`.password_${index}`).val();
+    console.log('submitted edit info form successfully');
+    console.log('from within ajax:', web_name, username, password);
+    $.post('/passwords/edit', { web_name, username, password })
+    .then((res) => {
+      $('.passwords_container').empty();
+      renderPasswords();
+    }).catch(err => {
+      console.log('error from edit password request:', err);
+    })
+  });
+  return;
 })
