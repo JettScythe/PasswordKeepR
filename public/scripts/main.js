cbe59c0fa296da52e801f5671dec662268fa7fe5
Zain Dewsi <zain.dewsi@gmail.com>
2021-06-01 10:49:31 -0400

    order passwords by id/name

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,10 +1,10 @@
-const renderPasswords = () => {$.ajax({
-    type: 'GET',
-    url: '/passwords/new',
-    success: function(passwords) {
-      // $('.passwords_container').empty();
+const renderPasswords = () => {
+  $.ajax({
+    type: "GET",
+    url: "/passwords/new",
+    success: function (passwords) {
       $.each(passwords, (index, password) => {
-        $('.passwords_container').append(`
+        $(".passwords_container").append(`
           <div class='display_password'>
             <header>${password.website_name}</header>
             <p>username: ${password.website_username}</p>
@@ -31,62 +31,54 @@ const renderPasswords = () => {$.ajax({
             </form>
           </div>
         `);
-      })
-    }
-  })
-}
+      });
+    },
+  });
+};
 
 renderPasswords();
 
-$('.toggle_add_account').on('click', () => {
-  return $('.add_account').slideToggle();
+$(".toggle_add_account").on("click", () => {
+  return $(".add_account").slideToggle();
 });
 
-$('.add_new').submit(event => {
-  const name = $('#web_name').val();
-  const username = $('#web_username').val();
-  const password = $('#new_password').val();
+$(".add_new").submit((event) => {
+  const name = $("#web_name").val();
+  const username = $("#web_username").val();
+  const password = $("#new_password").val();
   event.preventDefault();
-  $.post('/passwords/new', { name, username, password })
-  .then((res) => {
-    $('.passwords_container').empty();
-    renderPasswords();
-  }).catch(err => {
-    console.log('error from submit new password request:', err);
-  })
-})
+  $.post("/passwords/new", { name, username, password })
+    .then((res) => {
+      $(".passwords_container").empty();
+      renderPasswords();
+    })
+    .catch((err) => {
+      console.log("error from submit new password request:", err);
+    });
+});
 
-
-
-$('.passwords_container').on('click', '.toggle_edit', (event) => {
+$(".passwords_container").on("click", ".toggle_edit", (event) => {
   event.stopImmediatePropagation();
   event.preventDefault();
   $(event.currentTarget.nextElementSibling).slideToggle();
-  const website = event.target.parentElement.childNodes[1].innerText;
-  console.log(website);
 
-  const form = $(event.currentTarget.nextElementSibling).attr('class');
+  const form = $(event.currentTarget.nextElementSibling).attr("class");
   const index = form.slice(-1);
-  console.log('index:', index);
 
-  $('.passwords_container').on('submit', `.edit_info_${index}`, (event) => {
+  $(".passwords_container").on("submit", `.edit_info_${index}`, (event) => {
     event.stopImmediatePropagation();
     event.preventDefault();
-    console.log(website)
-    console.log(`this event is: ${event.currentTarget}`)
-    //const web_name = $(`.web_name_${index}`).val();
+    const website = event.target.parentElement.childNodes[1].innerText;
     const username = $(`.username_${index}`).val();
     const password = $(`.password_${index}`).val();
-    console.log('submitted edit info form successfully');
-    console.log('from within ajax:', website, username, password);
-    $.post('/passwords/edit', { website, username, password })
-    .then((res) => {
-      $('.passwords_container').empty();
-      renderPasswords();
-    }).catch(err => {
-      console.log('error from edit password request:', err);
-    })
+    $.post("/passwords/edit", { website, username, password })
+      .then((res) => {
+        $(".passwords_container").empty();
+        renderPasswords();
+      })
+      .catch((err) => {
+        console.log("error from edit password request:", err);
+      });
   });
-  console.log('++++++++++++++++');
   return;
-})
+});
--- routes/allPasswords.js
+++ routes/allPasswords.js
@@ -1,35 +1,39 @@
-const express = require('express');
-const router  = express.Router();
-const helper = require('../helpers');
+const express = require("express");
+const router = express.Router();
+const helper = require("../helpers");
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    db.query(`SELECT * FROM passwords WHERE organization_id = $1;`, [req.session['organization_id']])
-      .then(data => {
+    db.query(
+      `SELECT * FROM passwords WHERE organization_id = $1 ORDER BY catagory_id, website_name;`,
+      [req.session["organization_id"]]
+    )
+      .then((data) => {
         const passwords = data.rows;
         res.json(passwords);
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
       });
   });
 
-  router.post('/', (req, res) => {
-    console.log('it made it to here....', req.body);
+  router.post("/", (req, res) => {
+    console.log("it made it to here....", req.body);
     const body = req.body;
     const orgID = req.session.organization_id;
 
-    db.query(`INSERT INTO passwords (website_name, website_username, website_password, organization_id, catagory_id)
+    db.query(
+      `INSERT INTO passwords (website_name, website_username, website_password, organization_id, catagory_id)
       VALUES ($1, $2, $3, $4, $5)
       RETURNING *;`,
       [body.name, body.username, body.password, orgID, 1]
-    ).then(res => {
-      return res.rows;
-    }).catch(err => console.log('error from post request:', err));
+    )
+      .then((res) => {
+        return res.rows;
+      })
+      .catch((err) => console.log("error from post request:", err));
     // res.redirect('/passwords');
-    res.send('success');
+    res.send("success");
   });
 
   return router;
