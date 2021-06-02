aa9133e1f4b75082efbdd9cd22ca4d76ffb25803
JettScythe <jettscythe@hotmail.com>
2021-06-02 15:08:44 -0300

    fix class names

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -26,7 +26,7 @@ const renderPasswords = () => {
               <div class="form-group">
                 <input
                   type="text"
-                  class="updated_username_${index}"
+                  class="username_${index}"
                   name="website_username"
                   placeholder="username"
                 />
@@ -34,7 +34,7 @@ const renderPasswords = () => {
               <div class="form-group">
                 <input
                   type="password"
-                  class="updated_password_${index}"
+                  class="password_${index}"
                   name="website_password"
                   placeholder="password"
                 />
