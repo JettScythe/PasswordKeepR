723cb8853122a6a88a459736675a79861f26b028
Zain Dewsi <zain.dewsi@gmail.com>
2021-06-03 16:02:29 -0400

    code cleanup

--- routes/account_info.js
+++ routes/account_info.js
@@ -1,43 +1,45 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 module.exports = (db, obj) => {
-  router.get('/', (req, res) => {
+  router.get("/", (req, res) => {
     const user = req.session.user_id;
-    db.query(`SELECT users.*, organizations.name as org_name FROM users
+    db.query(
+      `SELECT users.*, organizations.name as org_name FROM users
     JOIN organizations ON organizations.id = organization_id
-    WHERE users.id = $1;`, [user])
-    .then(userInfo => {
-      const info = userInfo.rows[0];
-      info.user_id = info.id;
-      console.log(info);
-      res.render('account_info', info);
-    }).catch(err => {
-      console.log('error from /api/passwords/account_info:', err);
-    })
+    WHERE users.id = $1;`,
+      [user]
+    )
+      .then((userInfo) => {
+        const info = userInfo.rows[0];
+        info.user_id = info.id;
+        res.render("account_info", info);
+      })
+      .catch((err) => {
+        console.log("error from /api/passwords/account_info:", err);
+      });
   });
 
-  router.post('/email', (req, res) => {
+  router.post("/email", (req, res) => {
     const id = req.session.user_id;
     const { email } = req.body;
     obj.updateUserEmail(email, id);
     res.send();
-  })
+  });
 
-  router.post('/name', (req, res) => {
+  router.post("/name", (req, res) => {
     const id = req.session.user_id;
     const { name } = req.body;
-    console.log('the new name is:', name);
     obj.updateUserName(name, id);
     res.send();
-  })
+  });
 
-  router.post('/password', (req, res) => {
+  router.post("/password", (req, res) => {
     const id = req.session.user_id;
     const { password } = req.body;
     obj.updateUserPassword(password, id);
     res.send();
-  })
+  });
 
   return router;
-}
+};
